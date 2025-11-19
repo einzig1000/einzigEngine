ea344a56933bd1c03c0b9d997197645d0848495b
einzig1000 <k024g0089@g.neec.ac.jp>
2025-11-19 15:52:46 +0900

    a

--- src/application/Phase/TestPhase/TestPhase.cpp
+++ src/application/Phase/TestPhase/TestPhase.cpp
@@ -357,8 +357,9 @@ void TestPhase::Update()
 
 void TestPhase::Draw()
 {
-	rect_.Draw();
-	rect_.DrawImGui();
+	//rect_.Draw();
+	//rect_.DrawImGui();
+	
 	//ground_.Draw();
 	//ground_.DrawImGui();
 	//ground_.DrawAABB();
@@ -381,12 +382,12 @@ void TestPhase::Draw()
 	//triangle2_.Draw();
 	//triangle2_.DrawImGui();
 
-	//particle1_.Draw();
-	//particle1_.DrawImGui();
-	//particle1_.DrawEmitter();
-	//particle2_.Draw();
-	//particle2_.DrawImGui();
-	//particle2_.DrawEmitter();
+	particle1_.Draw();
+	particle1_.DrawImGui();
+	particle1_.DrawEmitter();
+	particle2_.Draw();
+	particle2_.DrawImGui();
+	particle2_.DrawEmitter();
 
 	//line_.Draw();
 	//line_.DrawImGui();
