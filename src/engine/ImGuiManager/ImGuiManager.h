97bb05db3144fb02bb605889bfaa639544f6278e
einzig1000 <k024g0089@g.neec.ac.jp>
2026-01-14 10:27:42 +0900

    imguiをexternals

--- src/engine/ImGuiManager/ImGuiManager.h
+++ src/engine/ImGuiManager/ImGuiManager.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#include "imgui.h"
-#include "imgui/imgui_impl_dx12.h"
-#include "imgui/imgui_impl_win32.h"
+#include "externals/imgui/imgui.h"
+#include "externals/imgui/imgui_impl_dx12.h"
+#include "externals/imgui/imgui_impl_win32.h"
 
 class DirectXManager;
 class WindowManager;
